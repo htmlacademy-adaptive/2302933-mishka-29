592865a15ceaa6965fe196eb29473bcb2ea1e409
Peter <petmeln04@gmail.com>
2023-12-15 18:02:31 +0200

    делает пиксель пёрфект и переполнение

--- source/js/main-navigation.js
+++ source/js/main-navigation.js
@@ -1,20 +1,20 @@
 // НАВИГАЦИЯ
 
 let pageHeader = document.querySelector('.page-header');
-let navList = document.querySelector('.main-navigation__list');
+let navLists = document.querySelector('.main-navigation__lists');
 let navToggle = document.querySelector('.main-navigation__toggle');
 
 pageHeader.classList.remove('page-header--nojs');
 
 navToggle.addEventListener('click', function () {
-  if (navList.classList.contains('main-navigation__list--closed')) {
-    navList.classList.remove('main-navigation__list--closed');
-    navList.classList.add('main-navigation__list--opened');
+  if (navLists.classList.contains('main-navigation__lists--closed')) {
+    navLists.classList.remove('main-navigation__lists--closed');
+    navLists.classList.add('main-navigation__lists--opened');
     navToggle.classList.remove('main-navigation__toggle--default');
     navToggle.classList.add('main-navigation__toggle--active');
   } else {
-    navList.classList.add('main-navigation__list--closed');
-    navList.classList.remove('main-navigation__list--opened');
+    navLists.classList.add('main-navigation__lists--closed');
+    navLists.classList.remove('main-navigation__lists--opened');
     navToggle.classList.add('main-navigation__toggle--default');
     navToggle.classList.remove('main-navigation__toggle--active');
   }
